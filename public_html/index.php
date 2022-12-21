ebcabc95e5a100f38f18382916d41c2c5dc0a771
Suphattra2002 <nb42481@satrinon.ac.th>
2022-12-21 17:59:38 +0700

    Update index.php

--- public_html/index.php
+++ public_html/index.php
@@ -1,1 +1,20 @@
-<h1>My PHP </h1>
+<html> 
+<head> 
+<title>โปรแกรมสูตรคูณด้วยคำสั่ง for</title> 
+</head> 
+<body> 
+<form method= "post"> 
+<p>กรุณากรอกแม่สูตรคูณที่ต้องการ : 
+<input name= "multi"> 
+<input name= "submit" type="submit" value="เลือก"> 
+</p> 
+</form> 
+<?php
+$multi = @$_POST['multi'] ; 
+echo "<centre>สูครคูณแม่ $multi <br>"; 
+for($i=1;$i<=12;$i++) 
+{ 
+echo "$i x $multi = ".$i*$multi. "<br>"; 
+} ?> 
+</body> </html>
+
